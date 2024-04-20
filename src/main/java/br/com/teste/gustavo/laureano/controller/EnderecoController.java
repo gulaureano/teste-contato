b5ce6f1ecbfb7093663a97b2f37f83ccbb8811bb
Gustavo Laureano <gustavolaureano3510@gmail.com>
2024-04-20 19:19:16 -0300

    Adicionando metodos CRUD do Endereco

--- src/main/java/br/com/teste/gustavo/laureano/controller/EnderecoController.java
+++ src/main/java/br/com/teste/gustavo/laureano/controller/EnderecoController.java
@@ -3,12 +3,19 @@ package br.com.teste.gustavo.laureano.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.teste.gustavo.laureano.dto.EnderecoAllDto;
+import br.com.teste.gustavo.laureano.dto.EnderecoDto;
 import br.com.teste.gustavo.laureano.service.EnderecoService;
 
 @RequestMapping("/endereco")
@@ -22,6 +29,27 @@ public class EnderecoController {
 	public ResponseEntity<List<EnderecoAllDto>> findAll(){
 		List<EnderecoAllDto> enderecos = service.findAll();
 		return ResponseEntity.ok(enderecos);
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> create(@RequestBody EnderecoDto enderecoCreate){
+		return ResponseEntity.ok(service.create(enderecoCreate));
+	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<String> update(@RequestBody EnderecoDto enderecoUpdate, @PathVariable Long id){
+		boolean alterado = service.update(enderecoUpdate, id);
+		if (alterado) {
+			return ResponseEntity.ok("Informações do endereco atualizadas com sucesso!");
+		} return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possível encontrar o endereco com esse ID: " + id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> delete(@PathVariable Long id){
+		boolean deletado = service.delete(id);
+		if (deletado) {
+			return ResponseEntity.ok("endereco deletado com sucesso!");
+		} return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possível encontrar o endereco");
 	}
 
 }
--- src/main/java/br/com/teste/gustavo/laureano/service/EnderecoService.java
+++ src/main/java/br/com/teste/gustavo/laureano/service/EnderecoService.java
@@ -2,12 +2,14 @@ package br.com.teste.gustavo.laureano.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.teste.gustavo.laureano.domain.Endereco;
 import br.com.teste.gustavo.laureano.dto.EnderecoAllDto;
+import br.com.teste.gustavo.laureano.dto.EnderecoDto;
 import br.com.teste.gustavo.laureano.repository.EnderecoRepository;
 
 @Service
@@ -35,6 +37,48 @@ public class EnderecoService {
 
 	public Endereco findById(Long idEndereco) {
 		return repository.getReferenceById(idEndereco);
+	}
+
+	public String create(EnderecoDto enderecoCreate) {
+		Endereco endereco = new Endereco();
+		endereco.setRua(enderecoCreate.getRua());
+		endereco.setNumero(enderecoCreate.getNumero());
+		endereco.setCep(enderecoCreate.getCep());
+		repository.save(endereco);
+		return "Novo endereço cadastrado com sucesso!";
+	}
+
+	public boolean update(EnderecoDto enderecoUpdate, Long id) {
+		Endereco endereco = findById(id);
+		if (endereco != null) {
+			if (enderecoUpdate.getRua() != null && !enderecoUpdate.getRua().isEmpty()
+					&& !enderecoUpdate.getRua().equals(endereco.getRua())) {
+				endereco.setRua(enderecoUpdate.getRua());
+			}
+			
+			if (enderecoUpdate.getNumero() != null && !enderecoUpdate.getNumero().isEmpty()
+					&& !enderecoUpdate.getNumero().equals(endereco.getNumero())) {
+				endereco.setNumero(enderecoUpdate.getNumero());
+			}
+			
+			if (enderecoUpdate.getCep() != null && !enderecoUpdate.getCep().isEmpty()
+					&& !enderecoUpdate.getCep().equals(endereco.getCep())) {
+				endereco.setCep(enderecoUpdate.getCep());
+			}
+			repository.save(endereco);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean delete(Long id) {
+		// TODO adicionar excecao de ConstraintViolantion ao tentar excluir endereco relacionado ao contato, alem de outras excecoes
+		Optional<Endereco> endereco = repository.findById(id);
+		if (endereco.isPresent()) {
+			repository.delete(endereco.get());
+			return true;
+		}
+		return false;
 	}
 
 }
